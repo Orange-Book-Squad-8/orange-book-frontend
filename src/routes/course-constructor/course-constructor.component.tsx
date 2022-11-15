b9d76049bf1c3bc8a6e19632c1deb2874b1d2f48
Yure Nogueira <1993nogueirayure@gmail.com>
2022-11-15 15:44:48 -0300

    feat: usuários são barrados de editar curso de outros usuários

--- src/routes/course-constructor/course-constructor.component.tsx
+++ src/routes/course-constructor/course-constructor.component.tsx
@@ -2,10 +2,16 @@ import { CourseEditDashboard } from '../../components/create-pannel/course-edit-
 import { AdminDashboardContainer } from './course-constructor.styles';
 import { useDispatch, useSelector } from 'react-redux';
 import { api } from '../../lib/axios';
-import { selectUser, setCourse, setSectionList } from '../../redux/reducers';
+import {
+  selectCourseList,
+  selectUser,
+  setCourse,
+  setSectionList
+} from '../../redux/reducers';
 import { CourseEditPanel } from '../../components/create-pannel/course-edit-panel';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { CourseDTO, Lesson } from '../../interfaces/api';
+import { useEffect } from 'react';
 
 interface ServerCourseResponse {
   courseDTO: CourseDTO;
@@ -22,52 +28,76 @@ function CourseConstructor() {
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
   const { courseId } = useParams() as { courseId: string };
+  const navigate = useNavigate();
+  const { myCourses } = useSelector(selectCourseList);
+  const amICreator = myCourses?.some(
+    (course) => course.id === Number(courseId)
+  );
 
+  useEffect(() => {
+    if (myCourses && !amICreator) {
+      navigate('/home');
+    }
+  }, [amICreator]);
 
   async function fetchLessons() {
     try {
       if (courseId == 'new') {
         const responseAllLessons = await api.get<Lesson[]>('/lessons/all');
 
-        dispatch(setSectionList([{
-          lessons: responseAllLessons.data,
-          name: 'adm lessons',
-          id: 0
-        }]));
+        dispatch(
+          setSectionList([
+            {
+              lessons: responseAllLessons.data,
+              name: 'adm lessons',
+              id: 0
+            }
+          ])
+        );
 
-        dispatch(setCourse({
-          id: NaN,
-          title: '',
-          creator: user.username,
-          visible: true,
-          totalLessons: 0,
-          difficulty: 'BEGINNER',
-          category: 'UX',
-          description: ''
-        }));
+        dispatch(
+          setCourse({
+            id: NaN,
+            title: '',
+            creator: user.username,
+            visible: true,
+            totalLessons: 0,
+            difficulty: 'BEGINNER',
+            category: 'UX',
+            description: ''
+          })
+        );
       } else {
         const responseAllLessons = await api.get<Lesson[]>('/lessons/all');
-        const responseCourse = await api.get<ServerCourseResponse>(`/courses/${courseId}`);
+        const responseCourse = await api.get<ServerCourseResponse>(
+          `/courses/${courseId}`
+        );
 
         let allLessons: number[] = [];
         const courseSections = responseCourse.data.completeSectionDTO;
-        courseSections.forEach(section => {
-          section.lessons.forEach(lesson => {
+        courseSections.forEach((section) => {
+          section.lessons.forEach((lesson) => {
             allLessons.push(lesson.id);
           });
         });
 
-        const admLessons: Lesson[] = responseAllLessons.data.filter(lesson => !allLessons.includes(lesson.id));
+        const admLessons: Lesson[] = responseAllLessons.data.filter(
+          (lesson) => !allLessons.includes(lesson.id)
+        );
 
-        dispatch(setSectionList([{
-          lessons: admLessons,
-          name: 'adm lessons',
-          id: 0
-        }, ...responseCourse.data.completeSectionDTO]));
+        dispatch(
+          setSectionList([
+            {
+              lessons: admLessons,
+              name: 'adm lessons',
+              id: 0
+            },
+            ...responseCourse.data.completeSectionDTO
+          ])
+        );
 
         dispatch(setCourse(responseCourse.data.courseDTO));
       }
-
     } catch (err) {
       console.error(err);
     }
