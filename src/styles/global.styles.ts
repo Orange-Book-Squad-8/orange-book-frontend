31646c2ea49958a1ab9bea8b4f480d7ccca0a6ca
MarceloRibeiro1 <86325217+MarceloRibeiro1@users.noreply.github.com>
2022-11-15 12:33:17 -0300

    style scrollbar

--- src/styles/global.styles.ts
+++ src/styles/global.styles.ts
@@ -13,6 +13,22 @@ export const GlobalStyles = createGlobalStyle`
     min-height: 100vh;
     font: 100%/1.5 ${({ theme }) => theme.ubuntu};
     background-color: ${({ theme }) => theme.primaryDarker};
+
+    &::-webkit-scrollbar {
+      background-color: ${({ theme }) => theme.primary};
+      width: 10px;
+      height: 10px;
+      border: 1px solid black;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      background-color: ${({ theme }) => theme.secondaryDarker};
+      width: 5px;
+      height: 5px;
+      border: 1px solid black;
+      border-radius: 3px;
+    }
+
   }
 
   div#root {
